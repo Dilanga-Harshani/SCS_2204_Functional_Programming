3ee8ce5b7a7c4980c69bcd0bbd36b2d779194f84
Dilanga-Harshani <84991000+Dilanga-Harshani@users.noreply.github.com>
2022-07-18 19:47:52 +0530

    Update 20000723_Q4.scala

--- Tutorial_3/20000723_Q4.scala
+++ Tutorial_3/20000723_Q4.scala
@@ -3,10 +3,22 @@
 
 object Q4 extends App {
 
-    //Deffining the function to find the wholesale cost when the no of copies is given
-    def cost (n_books : Int) : Double = (n_books * 24.95 * 0.6) + 3 + ((n_books - 50) * 0.75)
+    //Defining a function to calculate the cover price of books with discount
+    def book_price (n_books : Int) : Double = n_books * 24.95 * 0.6
+
+    //Defining a function to calculate the shipping cost
+    def ship_cost (n_books : Int) : Double = n_books <= 50 match {
+        //Using a match case to define the shipping cost based on no of copies    
+
+        case true => n_books * 3
+        case false => (n_books - 50) * 0.75 + 150
+
+    }
+
+    //Defining a function to calculate the total wholesale cost
+    def tot_cost (n_books : Int) : Double = book_price (n_books) + ship_cost (n_books)
     
-    //Calling the function with 60 as the no of copies and printing the result
-    println ("\nTotal wholesale cost for 60 copies = Rs. " + cost (60) + "\n")
+    //Calling the function with 60 as the no of copies and printing the wholesale cost
+    println ("\nTotal wholesale cost for 60 copies = Rs. " + tot_cost (60) + "\n")
 
 }
